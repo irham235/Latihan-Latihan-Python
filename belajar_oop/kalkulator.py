0079223b00783d849d22bed67d4c727a200563ff
irham235 <irhammaulana235@gmail.com>
2023-10-18 20:03:19 +0700

    coba ubah

--- belajar_oop/kalkulator.py
+++ belajar_oop/kalkulator.py
@@ -1,8 +1,11 @@
-class Kalkulator :
+class Kalkulator:
     """kalkulator tambah kurang"""
+
     def __init__(self, _i):
         self.i = _i
+
     def tambah(self, _i):
         return self.i + _i
+
     def kurang(self, _i):
         return self.i - _i
